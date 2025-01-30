b604fb6a4fb0e93f963848b992d62d97ee35aea1
MuhammedSalman1 <151732382+MuhammedSalman1@users.noreply.github.com>
2025-01-31 03:38:06 +0500

    Refresh and access token decode

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,8 @@
+import {asyncHandler} from "../utils/asyncHandler.js"
+
+export const verifyJWT = asyncHandler(async(req,res,next)=>{
+  const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer","")
+  if (!token) {
+    throw new ApiError (401 , "unauthorized access")
+  }
+})
